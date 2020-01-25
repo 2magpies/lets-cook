fa3c82262db2665eb98bb4271d229768d7bdec47
2magpies <daphnemintz@yahoo.com>
2020-01-25 00:10:08 -0500

    Add and style SuggestedPanel component JSX

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import DailyRecipe from './DailyRecipe';
 import SuggestedPanel from './SuggestedPanel';
 import './App.css';
 
+const images = [
+  {
+    id: 52768,
+    url: 'https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg'
+  },
+  {
+    id: 52893,
+    url: 'https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg'
+  }
+];
+
 function App() {
   return (
     <div>
       <SiteInfo />
       <DailyRecipe />
-      <SuggestedPanel />
+      <SuggestedPanel images={images} />
     </div>
   );
 }
--- src/SuggestedPanel.js
+++ src/SuggestedPanel.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-function SuggestedPanel() {
+
+
+
+const SuggestedPanel = ({ images }) => {
+  // return early if there are no images
+  if (!images.length) {
+    return <h2>No Images Found!</h2>;
+  }
+
   return (
-    <div>
-      <h1>Suggested Recipes Panel</h1>
+    <div className="gallery">
+      {images.map(image => (
+        <div key={image.id} className="gif">
+          <img src={image.url} alt={image.title} width="200"/>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default SuggestedPanel;
